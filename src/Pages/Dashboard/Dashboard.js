2ce91739884c0c0385bc7670a6a8ad44f63fd65a
huzaifa26 <abidhuzaifa128@gmail.com>
2022-08-24 20:25:03 +0500

    pages

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { URL } from "../../App";
 import PropgressModal from "./PropgressModal";
 
 export default function Dashboard(props){
+    let user;
+    user=localStorage.getItem("user");
+    user=JSON.parse(user);
+
+    const [users,setUsers]=useState([]);
+    const [managers,setManagers]=useState([]);
 
     const [showModal,setShowModal]=useState(false);
     const showModalHandler=()=>{
@@ -11,6 +19,54 @@ export default function Dashboard(props){
     const hideModalHandler=()=>{
         setShowModal(false);
     }
+
+    const userRoleHandler=useCallback((e,id)=>{
+        console.log(e.target.value);
+        console.log(id)
+        let data={
+            role:e.target.value,
+            id:id
+        }
+
+        axios.post(URL+"/user-role",data).then(res=>{
+            console.log(res.data.res);
+        }).catch(err=>{
+            console.log(err);
+        })
+    },[])
+
+    const assignManager=useCallback((e,id)=>{
+        console.log(e.target.value);
+        console.log(id)
+        let data={
+            mId:e.target.value,
+            uId:user.id
+        }
+
+        axios.post(URL+"/assign-manager",data).then(res=>{
+            console.log(res.data.res);
+        }).catch(err=>{
+            console.log(err);
+        })
+    },[])
+
+    useEffect(()=>{
+        axios.get(URL+"/user").then(res=>{
+            setUsers(res.data.res);
+        }).catch(err=>{
+            console.log(err);
+        })
+    },[userRoleHandler])
+
+
+    useEffect(()=>{
+        axios.get(URL+"/getManager").then(res=>{
+            setManagers(res.data.res);
+        }).catch(err=>{
+            console.log(err);
+        })
+    },[assignManager])
+
     return(
         <>
         {showModal &&
@@ -30,43 +86,37 @@ export default function Dashboard(props){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="flex my-[20px]">
-                        <td className="flex-1">Sung Po Yu</td>
-                        <td className="flex-1">1601 branham lane Suite 40 San J</td>
-                        <td className="flex-1">
-                            <select className="">
-                                <option selected>Franchise</option>
-                            </select>
-                        </td>
-                        <td className="flex-1"><button onClick={showModalHandler} className="bg-[#36c0f8] text-white h-[2.26vh] min-h-[30px] rounded-full w-[8vw]">Check Status</button></td>
-                        <td className="flex-1"><button className="bg-[#36f87a] text-white h-[2.26vh] min-h-[30px] rounded-full w-[10vw]">Check Message</button></td>
+                    {users.map((u)=>{
+                        if(u.id === user.id) return;
+                        return(
+                        <tr className="flex my-[20px]">
+                            <td className="flex-1">{u.userName}</td>
+                            <td className="flex-1">{u.desiredLocation}</td>
+                            <td className="flex-1">
+                                <select onChange={(e)=>userRoleHandler(e,u.id)} className="">
+                                    <option value={"admin"} selected={u.role === "admin"?true:false}>Admin</option>
+                                    <option value={"manager"} selected={u.role === "manager"?true:false}>Training Manager</option>
+                                    <option value={"franchisee"} selected={u.role === "franchisee"?true:false}>Franchisee</option>
+                                    <option value={"member"} selected={u.role === "member"?true:false}>Member</option>
+                                </select>
+                            </td>
+                            <td className="flex-1"><button onClick={showModalHandler} className="bg-[#36c0f8] text-white h-[2.26vh] min-h-[30px] rounded-full w-[8vw]">Check Status</button></td>
+                            <td className="flex-1"><button className="bg-[#36f87a] text-white h-[2.26vh] min-h-[30px] rounded-full w-[10vw]">Check Message</button></td>
 
-                        <td className="flex-1">
-                        <select className="">
-                                <option selected>Michelle young</option>
-                            </select>
-                        </td>
-                    </tr>
-
-                    <tr className="flex my-[20px]">
-                        <td className="flex-1">Sung Po Yu</td>
-                        <td className="flex-1">1601 branham lane Suite 40 San J</td>
-                        <td className="flex-1">
-                            <select className="">
-                                <option selected>Franchise</option>
-                            </select>
-                        </td>
-                        <td className="flex-1"><button onClick={showModalHandler} className="bg-[#36c0f8] text-white h-[2.26vh] min-h-[30px] rounded-full w-[8vw]">Check Status</button></td>
-                        <td className="flex-1"><button className="bg-[#36f87a] text-white h-[2.26vh] min-h-[30px] rounded-full w-[10vw]">Check Message</button></td>
-
-                        <td className="flex-1">
-                        <select className="">
-                                <option selected>Michelle young</option>
-                            </select>
-                        </td>
-                    </tr>
-
-
+                            <td className="flex-1">
+                                {managers.map((m)=>{
+                                    return(
+                                        <select onChange={assignManager} className="">
+                                            <option value={m.id} selected>{m.userName}</option>
+                                        </select>
+                                    )
+                                })}
+                                
+                            </td>
+                        </tr>
+                        )
+                    })}
+                    
 
                 </tbody>
                 </table>
